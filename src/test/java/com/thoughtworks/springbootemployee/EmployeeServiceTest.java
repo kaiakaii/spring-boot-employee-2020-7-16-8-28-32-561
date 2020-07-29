266aeab41d87c30b368e53068f4b033ab5f8eefa
SUNSK <senkk512@163.com>
2020-07-29 17:58:51 +0800

    [3-Restful API Practice In Spring-Boot][Sky,Cendeal] feature: add should_return_employees_when_find_all_given_no_parameters test

--- src/test/java/com/thoughtworks/springbootemployee/EmployeeServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/EmployeeServiceTest.java
@@ -1,4 +1,39 @@
 package com.thoughtworks.springbootemployee;
 
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import com.thoughtworks.springbootemployee.service.EmployeeService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class EmployeeServiceTest {
+    private static List<Employee> employees = new ArrayList<>();
+
+    @BeforeAll
+    static void init() {
+        for (int j = 0; j < 10; j++) {
+            employees.add(new Employee(j, "Tom" + j, 18, j % 2 == 0 ? "male" : "female", j * 1000));
+        }
+    }
+
+    @Test
+    void should_return_employees_when_find_all_given_no_parameters() {
+        //given
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        EmployeeService employeeService = new EmployeeService(employeeRepository);
+        when(employeeRepository.findAll()).thenReturn(employees);
+        //when
+        List<Employee> actualEmployees = employeeService.findAll();
+        //then
+        assertNotNull(actualEmployees);
+        assertEquals(employees.size(), actualEmployees.size());
+    }
 }
