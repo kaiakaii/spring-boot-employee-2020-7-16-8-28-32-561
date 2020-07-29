1f85e7214c67fde5b5f62d965b346d98032fdf19
SUNSK <senkk512@163.com>
2020-07-29 18:44:54 +0800

    [3-Restful API Practice In Spring-Boot][Sky,Cendeal] refactor: add generated value

--- src/main/java/com/thoughtworks/springbootemployee/model/Company.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Company.java
@@ -1,11 +1,14 @@
 package com.thoughtworks.springbootemployee.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Company {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String companyName;
     private int employeesNumber;
--- src/main/java/com/thoughtworks/springbootemployee/model/Employee.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Employee.java
@@ -1,12 +1,15 @@
 package com.thoughtworks.springbootemployee.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Objects;
 
 @Entity
 public class Employee {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
     private int age;
